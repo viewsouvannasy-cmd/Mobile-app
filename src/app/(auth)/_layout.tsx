f247c62520ee7ec54411a72120e534e1e826eac8
view <viewsouvannasy@gmail.com>
2026-09-16 15:10:06 +0700

    set up nativewind

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -2,8 +2,9 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(home)" />
+    <Stack>
+      <Stack.Screen name="sign-in" />
+      <Stack.Screen name="sign-up" />
     </Stack>
   );
 }
--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(home)" />
+    <Stack>
+      <Stack.Screen name="index" />
     </Stack>
   );
 }
